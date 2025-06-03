cc6d45f677e8088974b05f4dcac7e9afa8ab0f1d
zack173 <zalika@nycstudents.net>
2025-06-02 22:43:39 -0400

    Update taskModel.js

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -4,7 +4,7 @@ const db = require("../db");
 const getTasks = async () => {
   const res = await db.query(
     //correct this SQL query to select all tasks from the database
-    "SELECT everything FROM tasks ORDER BY created_at DESC"
+    "SELECT * FROM tasks ORDER BY created_at DESC"
   );
   return res.rows;
 };
