40f7bbb4360ee75eb9ca9afcae6564b1335c2b71
Jaybeesama <JUstinb515@nycstudents.net>
2025-06-03 11:57:27 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 // There is a bug in line 4 you need to fix it
 const taskModel = require("../models/taskModel");
-
+// the "models" was missing its s. Without the s, the code wouldn't function.
 //Write a comment describing the purpose of this route
 router.get("/", async (req, res) => {
   const tasks = await taskModel.getTasks();
@@ -14,6 +14,7 @@ router.post("/", async (req, res) => {
   //there is a bug in line 15 you need to fix
   const { name, description } = req.body;
   const task = await taskModel.addTask(name, description);
+//  const { name, description } addTask needs to match.
   res.status(201).json(task);
 });
 
